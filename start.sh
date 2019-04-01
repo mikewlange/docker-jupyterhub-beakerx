ad4cc15fe4e421aa52f0722d9aa34b820875d524
Geralt Roger Eric du Haute-Bellegarde <mike@mikelange.com>
2019-04-01 18:22:29 -0400

    Create start.sh

---
+++ start.sh
@@ -0,0 +1,8 @@
+set -e
+
+if [[ ! -z "${JUPYTERHUB_API_TOKEN}" ]]; then
+  # launched by JupyterHub, use single-user entrypoint
+  exec /usr/local/bin/start-singleuser.sh $*
+else
+  . /usr/local/bin/start.sh jupyter notebook $*
+fi
